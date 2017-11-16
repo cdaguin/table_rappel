95a0b8fccc6f02194b05248caffed3762faaf55d
Christophe Daguin <christophe.daguin@expandium.com>
2017-11-16 21:26:29 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rappel',
+      version='0.1',
+      description='Apprentissage des tables de rappels',
+      author='Christophe Daguin',
+      author_email='cdaguin@free.fr',
+      url='no url so far',
+      packages=['distutils', 'distutils.command'],
+     )
